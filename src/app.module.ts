1c4410c24cbedb996bf6df8f752c5896b0b9e2c6
gustavo <gustavo.ccx@hotmail.com>
2024-07-10 23:10:28 -0300

    INT-9
    - Arrumado o DELETE de Produto e Loja
    - Verifica se o ID existe na Tabela Produto Loja
    - Se o ID existir, deleta primeiro na Tabela Produto Loja e Depois em Produto
      ou Loja
    - CORS Habilitado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Loja } from './Model/Entity/Loja';
 import { Produto } from './Model/Entity/Produto';
